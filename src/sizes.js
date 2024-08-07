3e9ea6d76ab7890ac78f5fe22d660ee12c3c0a7b
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-08-06 21:37:03 -0500

    console log the width

--- src/sizes.js
+++ src/sizes.js
@@ -1,6 +1,6 @@
 var docEl = document.documentElement;
 var pageWidth = docEl.clientWidth;
-alert(pageWidth);
+console.log(pageWidth);
 var hh = document.querySelector("header").clientHeight;
 var mh = document.querySelector("main").clientHeight;
 var fh = document.querySelector("footer").clientHeight;
