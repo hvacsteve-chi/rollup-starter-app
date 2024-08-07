174a8bb30f94cd8a40be7bbc8447043884b57345
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-08-06 19:01:41 -0500

    sizes

---
+++ src/sizes.js
@@ -0,0 +1,7 @@
+var headerHeight = document.querySelector("header");
+
+var headerSize = document.getElementById("headerHeight");
+
+export function sizes(){
+  headerSize.innerHTML = headerHeight;
+}
