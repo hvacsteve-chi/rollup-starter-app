d37e03589c80776f656673e359e8c74c9b38b0ee
Stephen <59994443+hvacsteve-chi@users.noreply.github.com>
2024-08-06 20:40:56 -0500

    working with sizes

--- src/sizes.js
+++ src/sizes.js
@@ -1,7 +1,12 @@
-var headerHeight = document.querySelector("header");
+var hh = document.querySelector("header").clientHeight;
+var mh = document.querySelector("main").clientHeight;
+var fh = document.querySelector("footer").clientHeight;
+var headerSpan = document.querySelector("#headerHeight");
+var mainSpan = document.querySelector("#mainHeight");
+var footerSpan = document.querySelector("#footerHeight");
 
-var headerSize = document.getElementById("headerHeight");
-
-export function sizes(){
-  headerSize.innerHTML = headerHeight;
+export default function sizes(){
+  headerSpan.innerText = "hh: " + hh;
+  mainSpan.innerText = "mh: " + mh;
+  footerSpan.innerText = "fh: " + fh;
 }
